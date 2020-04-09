7d4469362d93094495bb005b684c0caca2c48d4b
Kamil Seweryn <sewerk@gmail.com>
2020-04-09 22:42:34 +0200

    Extract dependency versions to buildSrc

---
+++ buildSrc/src/main/kotlin/versions.kt
@@ -0,0 +1,14 @@
+const val kotlinVersion = "1.3.71"
+const val junit = "4.13"
+
+object Androidx {
+    const val appcompat = "1.1.0"
+    const val core = "1.2.0"
+    const val constraintlayout = "1.1.3"
+    const val lifecycle = "2.2.0"
+
+    object Test {
+        const val junit = "1.1.1"
+        const val espresso = "3.2.0"
+    }
+}
