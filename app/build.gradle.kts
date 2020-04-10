ddd6caa6f05e404f1269f3e09f27ea16ab419905
Kamil Seweryn <sewerk@gmail.com>
2020-04-10 23:06:27 +0200

    Extract dependency versions to buildSrc
    This reverts commit 24978abd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,14 +30,15 @@ android {
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${KotlinCompilerVersion.VERSION}")
-    implementation("androidx.appcompat:appcompat:1.1.0")
-    implementation("androidx.core:core-ktx:1.2.0")
-    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
-    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
 
-    testImplementation("junit:junit:4.13")
+    implementation("androidx.appcompat:appcompat:${Androidx.appcompat}")
+    implementation("androidx.core:core-ktx:${Androidx.core}")
+    implementation("androidx.constraintlayout:constraintlayout:${Androidx.constraintlayout}")
+    implementation("androidx.lifecycle:lifecycle-extensions:${Androidx.lifecycle}")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:${Androidx.lifecycle}")
 
-    androidTestImplementation("androidx.test.ext:junit:1.1.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+    testImplementation("junit:junit:$junit")
+
+    androidTestImplementation("androidx.test.ext:junit:${Androidx.Test.junit}")
+    androidTestImplementation("androidx.test.espresso:espresso-core:${Androidx.Test.espresso}")
 }
