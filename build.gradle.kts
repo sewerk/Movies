6f28aa936f5c40ea41f4734dd83b245a44f87d8a
Kamil Seweryn <sewerk@gmail.com>
2020-04-10 21:53:16 +0200

    Extract dependency versions to properties file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    val kotlinVersion = "1.3.71"
+    val kotlinVersion: String by project
     repositories {
         google()
         jcenter()
