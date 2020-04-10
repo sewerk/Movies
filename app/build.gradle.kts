500d4f6ec19346d0c85f24b725e2c33a47595ab9
Kamil Seweryn <sewerk@gmail.com>
2020-04-10 22:14:38 +0200

    Revert "Extract dependency versions to properties file"
    This reverts commit 6f28aa93

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,26 +27,17 @@ android {
     }
 }
 
-val junitVersion: String by project
-val appcompatVersion: String by project
-val androidCoreVersion: String by project
-val constraintlayoutVersion: String by project
-val lifecycleVersion: String by project
-val androidTestVersion: String by project
-val espressoVersion: String by project
-
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${KotlinCompilerVersion.VERSION}")
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.core:core-ktx:1.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
 
-    implementation("androidx.appcompat:appcompat:$appcompatVersion")
-    implementation("androidx.core:core-ktx:$androidCoreVersion")
-    implementation("androidx.constraintlayout:constraintlayout:$constraintlayoutVersion")
-    implementation("androidx.lifecycle:lifecycle-extensions:$lifecycleVersion")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    testImplementation("junit:junit:4.13")
 
-    testImplementation("junit:junit:$junitVersion")
-
-    androidTestImplementation("androidx.test.ext:junit:$androidTestVersion")
-    androidTestImplementation("androidx.test.espresso:espresso-core:$espressoVersion")
+    androidTestImplementation("androidx.test.ext:junit:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    val kotlinVersion: String by project
+    val kotlinVersion = "1.3.71"
     repositories {
         google()
         jcenter()
