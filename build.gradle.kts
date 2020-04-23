f60ccd7a267e286b00ac998004f7bdf836d6b16f
Kamil Seweryn <sewerk@gmail.com>
2020-04-23 21:55:10 +0200

    Update gradle plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
 buildscript {
     repositories {
         google()
@@ -7,11 +5,8 @@ buildscript {
 
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.6.2")
+        classpath("com.android.tools.build:gradle:3.6.3")
         classpath(kotlin("gradle-plugin", version = kotlinVersion))
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
     }
 }
 
