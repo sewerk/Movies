9a39fd05660612346a81aeb2ad84ad17354220a1
Kamil Seweryn <sewerk@gmail.com>
2020-07-07 13:42:01 +0200

    Enable strict mode

--- app/src/main/java/pl/srw/movies/MoviesApp.kt
+++ app/src/main/java/pl/srw/movies/MoviesApp.kt
@@ -1,6 +1,7 @@
 package pl.srw.movies
 
 import android.app.Application
+import android.os.StrictMode
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -13,7 +14,16 @@ class MoviesApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+            StrictMode.enableDefaults()
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectNetwork()
+                    .penaltyDeath()
+                    .build()
+            )
+        }
 
         startKoin {
             if (BuildConfig.DEBUG) androidLogger()
