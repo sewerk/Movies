576ac0aca35ed1eeda4fab4330189de066bdec00
Kamil Seweryn <sewerk@gmail.com>
2020-07-08 12:13:28 +0200

    Remove warning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         }
     }
 
-    dataBinding.isEnabled = true
+    buildFeatures.dataBinding = true
 }
 
 dependencies {
--- buildSrc/src/main/kotlin/android-kotlin-library.gradle.kts
+++ buildSrc/src/main/kotlin/android-kotlin-library.gradle.kts
@@ -25,7 +25,7 @@ android {
         }
     }
 
-    dataBinding.isEnabled = true
+    buildFeatures.dataBinding = true
 }
 
 dependencies {
